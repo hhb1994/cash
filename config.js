904f4ad2e30de85668909bb1a37fdd8cb2b89234
hanhongbin <hanhb1994924@qq.com>
2018-07-23 15:46:50 +0800

    new

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     mysql: {
         host: 'localhost',
         user: 'root',
-        password: '123456',
+        password: 'hhb123456',
         database: 'cash',
         port: 3306
     }
